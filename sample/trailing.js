69e2c5cb54b3606f7d7415b39f272c1183a6e63e
Sho Hashimoto <hashimoto@shokai.org>
2017-12-10 23:15:40 +0900

    add trailing sample

---
+++ sample/trailing.js
@@ -0,0 +1,22 @@
+require('babel-polyfill')
+
+const asyncSingleton = require('../')
+
+const delay = msec => new Promise(resolve => setTimeout(resolve, msec))
+
+async function countUp (n = 0) {
+  for (let i = 0; i < 10; i++) {
+    console.log(n + i)
+    await delay(100)
+  }
+}
+
+(async () => {
+  console.log('trailing: true')
+  const singleCountUp = asyncSingleton(countUp, {trailing: true})
+
+  singleCountUp(0) // run
+  singleCountUp(10) // skip
+  singleCountUp(20) // skip
+  singleCountUp(30) // run after "sinleCountUp(0)"
+})()
